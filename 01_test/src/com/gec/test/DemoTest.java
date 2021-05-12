75c5eb3c0b780f253804b1f7bfa29042255e6a19
zuoxiongxiong <83945901+zuoxiongxiong@users.noreply.github.com>
2021-05-12 11:25:45 +0800

    Update DemoTest.java

--- 01_test/src/com/gec/test/DemoTest.java
+++ 01_test/src/com/gec/test/DemoTest.java
@@ -4,7 +4,7 @@ public class DemoTest {
     public static void main(String[] args) {
         System.out.println("11111111111111111111111111111111111111");
 
-
+        System.out.println("111222222211111111111111111111111");
         System.out.println("------------------------------------------");
     }
 }
